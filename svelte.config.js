6535a8fcfc160023d1c51445c7912482d656fc2a
ryanjayleyva <ryanjayleyva@proton.me>
2024-12-17 03:04:52 -0500

    Added Shiki config

--- svelte.config.js
+++ svelte.config.js
@@ -5,18 +5,29 @@ import rehypeSlug from 'rehype-slug'
 import { mdsvex, escapeSvelte } from 'mdsvex'
 import { createHighlighter } from 'shiki/index.mjs'
 
+let cachedHighlighter
+
+async function getHighlighter() {
+	if (!cachedHighlighter) {
+		cachedHighlighter = await createHighlighter({
+			themes: ['poimandres'],
+			langs: ['javascript', 'typescript']
+		})
+	}
+	return cachedHighlighter
+}
+
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
-	// ...
 	remarkPlugins: [[remarkToc, { tight: true }]],
 	rehypePlugins: [rehypeSlug],
 	extensions: ['.md'],
+	layout: {
+		_: './src/mdsvex.svelte'
+	},
 	highlight: {
 		highlighter: async (code, lang = 'text') => {
-			const highlighter = await createHighlighter({
-				themes: ['poimandres'],
-				langs: ['javascript', 'typescript']
-			})
+			const highlighter = await getHighlighter()
 			await highlighter.loadLanguage('javascript', 'typescript')
 			const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'poimandres' }))
 			return `{@html \`${html}\` }`
